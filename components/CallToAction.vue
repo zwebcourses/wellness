27583ee8d68bd1662a0f53b8ef2c8832ae57e587
zwebcourses <dev.mahmoud.adel@gmail.com>
2023-01-24 21:30:11 +0200

    fix header text

--- components/CallToAction.vue
+++ components/CallToAction.vue
@@ -1,17 +1,22 @@
 <template>
-  <section
-    class="w-full overflow-hidden"
-  >
+  <section class="w-full overflow-hidden">
     <TheHeader v-on:hideLabel="(status) => hideLabelFun(status)" />
-    <div class="flex justify-center items-end md:h-screen h-[500px] bg-header bg-cover bg-center bg-no-repeat">
+    <div
+      class="flex justify-center items-end md:h-screen h-[500px] bg-header bg-cover bg-center bg-no-repeat"
+    >
       <!-- Header Text -->
-      <div class="lg:w-2/5 md:3/5 w-11/12 flex flex-col justify-center items-center">
+      <div
+        class="lg:w-3/5 md:4/5 w-11/12 flex flex-col justify-center items-center"
+      >
         <div class="w-full text-center md:p-4">
           <h1 class="md:text-5xl text-xl font-extrbold text-gold mb-4">
             الثروة الأولى هي الصحّة
           </h1>
-          <p class="md:text-3xl text-xl text-white font-extrbold">
-            أعظم هدية يمكنك تقديمها لعائلتك والعالم هtو أنك بصحة جيدة
+          <p class="md:text-3xl text-lg text-white font-extrbold">
+            أعظم هدية يمكنك تقديمها لعائلتك والعالم
+          </p>
+          <p class="md:text-3xl text-lg text-white font-extrbold">
+            هو أنك بصحة جيدة
           </p>
         </div>
         <div class="md:w-2/3 w-1/3 md:mb-8">
